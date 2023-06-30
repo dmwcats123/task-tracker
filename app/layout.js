2ebf155f8967db01cf9ca570f6ae6ab9a6c98b53
dmwcats <dmwcats123@gmail.com>
2023-06-30 14:25:51 -0500

    updated metadata

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,17 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Task Tracker',
-  description: 'The best task tracker ever made!',
-}
+  title: "Task Tracker",
+  description: "The best task tracker ever!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
